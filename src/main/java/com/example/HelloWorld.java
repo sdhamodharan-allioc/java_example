ffbc8a0f93b38281dddc9661e798fbe447210b7b
Savithri Dhamodharan <sdhamodharan@allioc.com>
2023-03-14 23:06:56 -0400

    Example project for Maven

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class HelloWorld {
+    public void sayhello()
+    {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/com/example/TestHelloWorld.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.Test;
+
+public class TestHelloWorld {
+    @Test
+    public void testSayHello()
+    {
+       HelloWorld hw = new HelloWorld();
+       hw.sayhello();
+    }
+}
